946cb5d56d0c40fd2e06db3b0c8cb73c140360a2
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-02-26 19:34:47 +0700

    initial commit - struktur folder - API konfigurasi

---
+++ README.md
@@ -0,0 +1,3 @@
+# money_records
+
+A new Flutter project.
---
+++ lib/config/api.dart
@@ -0,0 +1,6 @@
+class Api{
+  static const baseURL = 'http://192.168.1.7/api_money_record';
+
+  static const users = '$baseURL/users';
+  static const history = '$baseURL/history';
+}
