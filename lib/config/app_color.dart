0f9d454c5000d906a47653e0e3fd6cee83c47dda
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-02-26 19:50:58 +0700

    konfigurasi asset dan color

---
+++ lib/config/app_color.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+  static const primary = Color(0XFF767AE7);
+  static const secondary = Color(0XFF89FFFC);
+  static const bg = Color(0XFF9A83F5);
+  static const chart = Color(0XFFA308F4);
+}
