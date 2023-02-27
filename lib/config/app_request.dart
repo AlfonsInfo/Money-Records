9d3d889ccabd9ad431acfd2a1722043519317425
Alfonsdev <106644852+AlfonsInfo@users.noreply.github.com>
2023-02-27 16:47:41 +0700

    -template request app

---
+++ lib/config/app_request.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:d_method/d_method.dart';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+class AppRequest{
+
+  //Fungsi Get
+  static Future<Map?> gets(String url,{Map<String, String>? headers}) async{
+    try{
+      // var response = await http.get(Uri.parse(url),headers: headers);
+      http.Response response = await http.get(Uri.parse(url),headers: headers);
+      // if(Response base status code sabi)
+      DMethod.printTitle('try = $url', response.body);
+
+      Map responseBody = jsonDecode(response.body);
+      return responseBody; 
+    } catch(e){
+      DMethod.printTitle('catch', e.toString());
+      return null;
+    }
+  }
+
+  //Fungsi Post
+  static Future<Map?> post(String url,Object? body,{Map<String, String>? headers}) async{
+    try{
+      // var response = await http.get(Uri.parse(url),headers: headers);
+      http.Response response = await http.post(
+        Uri.parse(url),
+        body: body,
+        headers: headers);
+      // if(Response base status code sabi)
+      DMethod.printTitle('try = $url', response.body);
+      Map responseBody = jsonDecode(response.body);
+      return responseBody; 
+    } catch(e){
+      DMethod.printTitle('catch', e.toString());
+      return null;
+    }
+  }
+
+}
